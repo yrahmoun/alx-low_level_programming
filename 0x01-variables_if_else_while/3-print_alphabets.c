8dfb34721f60032dea6e89e645d5f81ea43a560c
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-11 03:53:14 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+   * main - print alphabet
+   *
+   *Return: zero
+   */
+
+int main(void)
+{
+        char a = 'a';
+	char A = 'A';
+
+        while (a <= 'z')
+        {
+                putchar(a);
+                a++;
+	}
+	while (A <= 'Z')
+        {
+                putchar(A);
+                A++;
+        }
+	putchar('\n');
+        return (0);
+}
