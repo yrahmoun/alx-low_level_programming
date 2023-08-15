2b4e8cc3e97990dfb918a74ee1bb4e4a5306ac0a
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-15 05:41:17 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
