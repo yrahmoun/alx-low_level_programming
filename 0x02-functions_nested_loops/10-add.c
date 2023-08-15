e24d6904521c065580f2adce64d1c80cdb32b4bc
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-15 06:20:46 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - calculate the sum
+ * @a: num 1
+ * @b: num 2
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
