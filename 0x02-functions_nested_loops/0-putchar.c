4e6cec3ed8ef62a8aa1f62cb0c0e3d63e14e898b
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-15 03:52:36 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
