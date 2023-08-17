08dc2658576699c8a13534214b1e507b099a405d
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-17 07:47:46 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + 48);
+		i++;
+	}
+	_putchar('\n');
+}
