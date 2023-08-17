2f72d5420ae8f8f7c80575720411deafce356b4b
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-17 08:06:35 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n = 0;
+
+	while (n < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i < 10)
+				_putchar(i + 48);
+			else
+			{
+				_putchar(i / 10 + 48);
+				_putchar(i % 10 + 48);
+			}
+			i++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
