e0281cac626d14627abb6001981ff8d79b4f82ca
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-23 10:14:44 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - turns string to uppersize
+ * @s: string
+ * Return: string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
