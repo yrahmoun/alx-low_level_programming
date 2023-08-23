1483915b1d9e6006d07b88df49a41abc85fd6ba9
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-23 09:48:05 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n characters from src to dest
+ * @src: source string
+ * @dest: destination string
+ * @n: number of characters
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
