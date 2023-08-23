1cecf7d8e246bd0dee32822a58e733ec2e13e67e
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-23 09:34:02 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string
+ * @dest: destination string
+ * @src! source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n = 0;
+	int i = 0;
+
+	while (dest[n])
+		n++;
+	while (src[i])
+	{
+		dest[n + i] = src[i];
+		i++;
+	}
+	dest[n + i] = '\0';
+	return (dest);
+}
