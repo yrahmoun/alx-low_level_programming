8a6c5b50a1cba120a28d1db3301dc9cb2fd2569f
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-08-23 11:29:19 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,22 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	char p[] = ",;.!?\"(){}";
+	int j;
 
 	while (s[i])
 	{
 		if (s[0] >= 'a' && s[0] <= 'z')
 			s[0] -= 32;
-		if (i > 0 && s[i - 1] <= 32)
+		j = 0;
+		while (p[j])
 		{
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] -= 32;
+			if ((i > 0 && s[i - 1] <= 32) || s[i] == p[j])
+			{
+				if (s[i] >= 'a' && s[i] <= 'z')
+					s[i] -= 32;
+			}
+			j++;
 		}
 		i++;
 	}
