0049c2f71fb7892b544027c7ff5e571b2a667b14
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-01 04:34:58 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: argument number
+ * @argv: argument array
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
