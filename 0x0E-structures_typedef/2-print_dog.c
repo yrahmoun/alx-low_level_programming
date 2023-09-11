d8da68ad09eda5efc6b7f314a23c96b2d34ac803
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-11 05:12:36 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		if (d->age)
+		if (d->age >= 0)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: (nil)");
