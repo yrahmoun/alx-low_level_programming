7ab9a385ff3a5052ca7f37180f0158a5fe48b1fd
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-11 04:24:07 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new struct
+ * @name: string holding dog name
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
