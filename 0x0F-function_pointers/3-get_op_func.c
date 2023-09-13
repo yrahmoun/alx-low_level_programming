0522bc0672a0a6a8357b0c6fbf187d9a2ef7c5fc
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-13 12:37:16 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <string.h>
 
 /**
  * get_op_func - get the function to execute
@@ -20,9 +19,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (s && !strcmp(ops[i].op, s) && !s[1])
+		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
 		i++;
 	}
