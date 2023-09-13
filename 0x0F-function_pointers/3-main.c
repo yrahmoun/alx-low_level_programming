6d04d3b98728c2fd8b5817308f9fe55595886b2d
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-13 09:13:17 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,18 +17,13 @@ int main(int ac, char **av)
 		printf("Error\n");
 		exit(98);
 	}
-	if (av[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	op = get_op_func(av[2]);
 	if (!op)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	res = get_op_func(av[2])(atoi(av[1]), atoi(av[3]));
+	res = op(atoi(av[1]), atoi(av[3]));
 	printf("%d\n", res);
 	return (0);
 }
