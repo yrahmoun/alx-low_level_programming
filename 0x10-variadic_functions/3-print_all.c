08a7ff2b22588855a7cf6cb8abcff1045e21c066
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-14 04:47:31 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: type of parameters passed
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	va_list p;
+	char *s;
+	int flag;
+
+	va_start(p, format);
+	while (format[i])
+	{
+		flag = 0;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(p, int));
+				flag = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(p, int));
+				flag = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(p, double));
+				flag = 1;
+				break;
+			case 's':
+				s = va_arg(p, char *);
+				if (!s)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				flag = 1;
+				break;
+		}
+		if (format[i + 1] && flag)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
