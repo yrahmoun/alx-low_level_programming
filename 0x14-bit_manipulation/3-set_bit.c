69030a752c046708e33cb7e2498248f61e4d3a58
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-28 04:58:55 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets bit at index to 1;
+ * @n: number
+ * @index: index
+ * Return: 1 or -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int shifts;
+	
+	shifts = sizeof(unsigned long int) * 8 - 1;
+	if (index > shifts)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
+}
