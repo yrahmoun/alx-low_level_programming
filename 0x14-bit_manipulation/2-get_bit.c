2c0236c5bd732b0ad3ff767a5bd8e29812e10d0a
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-09-28 04:37:19 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,15 +9,9 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int shifts = 0;
-	unsigned long int num = n;
+	unsigned int shifts;
 
-	while (num)
-	{
-		shifts++;
-		num >>= 1;
-	}
-	shifts--;
+	shifts = sizeof(unsigned long int) * 8 - 1;
 	if (index > shifts)
 		return (-1);
 	return ((n >> index) & 1);
