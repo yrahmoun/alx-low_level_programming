a9fbb993caef30efc38473237c128a49b55975b7
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-09 06:20:25 -0800

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			free(curr);
 			return (1);
 		}
-		if (count == index && curr->next)
+		if (count == index)
 		{
 			curr->prev->next = curr->next;
 			if (curr->next != NULL)
