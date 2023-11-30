74ae185e7dcff4e7739f182f0de4b3cc548a426a
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-30 07:42:59 -0800

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,15 +10,20 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i = 0;
 	hash_node_t *aux;
 
+	if (!ht)
+		return;
 	while (i < ht->size)
 	{
-		while (ht->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			aux = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = aux;
+			while (ht->array[i])
+			{
+				aux = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = aux;
+			}
 		}
 		i++;
 	}
